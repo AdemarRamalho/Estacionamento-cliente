c01cf6340a244cd7ef5e630c0af21d7647445c70
Ademar Ramalho <ademar.neto@descomplica.com.br>
2023-06-13 21:07:44 -0300

    app vue att

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
-<nav class="navbar navbar-expand-lg bg-warning nav-underline">
+<nav class="navbar navbar-expand-lg bg-success nav-underline">
   <div class="container-fluid">
    
-    <a class="navbar-brand" href="#">SpeedPark</a>
+    <a class="navbar-brand" href="#">PedroParking</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
