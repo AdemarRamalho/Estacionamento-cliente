ed8b9d4eef6f89d8dd8409c164e61dbf7c6da98b
Ademar Ramalho <ademar.neto@descomplica.com.br>
2023-06-14 21:30:30 -0300

    hotfix

--- src/App.vue
+++ src/App.vue
@@ -44,12 +44,6 @@
 </template>
 
 <style lang="scss">
-$primary: #900;
-$enable-shadows: true;
-$prefix: "mo-";
-
-// Then import Bootstrap
-@import "../node_modules/bootstrap/scss/bootstrap";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
