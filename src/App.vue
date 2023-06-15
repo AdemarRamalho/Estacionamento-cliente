1f120782d8b82876302b30609de7f6e02964abb9
Ademar Ramalho <ademar.neto@descomplica.com.br>
2023-06-15 12:07:45 -0300

    Criando a View condutor

--- src/App.vue
+++ src/App.vue
@@ -11,15 +11,15 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0 ">
         <li class="nav-item menu-gap">
-          <a class="nav-link active" aria-current="page" href="#">Menu</a>
+          <a class="nav-link active" id="cor-texto" aria-current="page" href="#">Menu</a>
         </li>
 
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" >
+          <a class="nav-link dropdown-toggle" id ="cor-texto" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" >
             Itens
           </a>
           <ul class="dropdown-menu" >
-            <li><a  class="dropdown-item" href="#">Movimentações</a></li>
+            <li><routerLink to="/movimentacoes"><a  class="dropdown-item">Movimentações</a></routerLink></li>
             <li><hr class="dropdown-divider"></li>
             <li><a  class="dropdown-item" href="#">Condutores</a></li>
             <li><a  class="dropdown-item" href="#">Veiculos</a></li>
@@ -33,7 +33,24 @@
     </div>
   </div>
 </nav>
-
+<form>
+  <div class="row">
+  <div class="form-group col-md-4 offset-md-4">
+    <label for="name">Nome</label>
+    <input type="name" class="form-control" id="name" aria-describedby="emailHelp" placeholder="Nome">
+  </div>
+  <div class="form-group col-md-4 offset-md-4">
+    <label for="email">Email</label>
+    <input type="name" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Digite seu Email">
+  </div>
+  <div class="form-group col-md-4 offset-md-4">
+    <label for="Senha">Senha</label>
+    <input type="password" class="form-control" id="Senha" placeholder="senha">
+  </div>
+  <button type="submit" class="btn btn-primary col-md-1 offset-md-4"></button>
+  <button type="submit" class="btn btn-primary col-md-1 offset-md-7">Voltar</button>
+</div>
+</form>
 
 
 </template>
@@ -54,7 +71,7 @@ nav {
     color: #ffffff;
 
     &.router-link-exact-active {
-      color:#42b983;
+      color:#f7f7f7;
     }
   }
 
@@ -69,5 +86,8 @@ nav {
     width: 40%;
     right: 50%;
     margin-left: 30%;
+  }
+  #cor-texto {
+    color: white;
   }
 </style>
